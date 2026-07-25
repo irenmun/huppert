614045cae9e861d70aefcecbbb2e377c2ce8bbb8
irenmun <irenmun@ukr.net>
2026-07-25 20:04:09 +0300

    Add Preview class to display rename previews

---
+++ renamer/preview.py
@@ -0,0 +1,21 @@
+class Preview:
+
+    def show(
+
+        self,
+
+        renamed
+
+    ):
+
+        print()
+
+        print("Rename Preview\n")
+
+        for old, new in renamed:
+
+            print(
+
+                f"{old}  ->  {new}"
+
+            )
