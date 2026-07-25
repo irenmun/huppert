130b212a816c15359826e0b5ab40a852a0c3bc9e
irenmun <irenmun@ukr.net>
2026-07-25 20:04:51 +0300

    Refactor main.py to include configuration imports

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
-""" Batch file renamer """
+from config import PREFIX
+from config import START_INDEX
+from config import REPORT_FILE
 
-print("Project: file-renamer")
+from renamer.scanner import Scanner
+from renamer.engine import RenameEngine
+from renamer.formatter import Formatter
+from renamer.preview import Preview
+
+from output.report import Report
+
+files = Scanner().scan()
+
+renamed = RenameEngine().rename(
+
+    files,
+
+    PREFIX,
+
+    START_INDEX,
+
+    Formatter()
+
+)
+
+Preview().show(
+
+    renamed
+
+)
+
+Report().save(
+
+    renamed,
+
+    REPORT_FILE
+
+)
